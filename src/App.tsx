e2011bfafdd0dba1e8fbacdb7361df74f10027b0
nikola <nikola.mihajlovic@eyesee-research.com>
2022-10-09 17:33:04 +0200

    feat: added dark theme, added not found page;

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,14 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { NotFound } from "./components/NotFound/NotFound";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
   const redirectToLogin = (
@@ -10,18 +20,25 @@ function App() {
   );
 
   return (
-      <Routes>
-        <Route path="/" element={redirectToLogin} />
-        <Route path="/login" element={<h1>LOGIN</h1>} />
-        <Route
-          path="overview/:category"
-          element={<h1>TESTIRANJE KATEGORIJE</h1>}
-        />
-        <Route
-          path="category/:category/:detail_id"
-          element={<h1>TESTIRANJE KATEGORIJE I PROJECT DETAILS</h1>}
-        />
-      </Routes>
+    <ThemeProvider theme={darkTheme}>
+      <Container maxWidth={false} disableGutters={true}>
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={redirectToLogin} />
+          <Route path="/login" element={<h1>LOGIN</h1>} />
+          <Route
+            path="overview/:category"
+            element={<h1>TESTIRANJE KATEGORIJE</h1>}
+          />
+          <Route
+            path="category/:category/:detail_id"
+            element={<h1>TESTIRANJE KATEGORIJE I PROJECT DETAILS</h1>}
+          />
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Container>
+    </ThemeProvider>
   );
 }
 
